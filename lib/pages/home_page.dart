71c982f5f019dfc7263207ff05cfc02c5f0c9185
Maleesha Hirushan <maleeshahir@gmail.com>
2026-06-03 09:04:16 +0530

    AppBar Title

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,27 +5,46 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          leading: Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: Container(
-              width: 30,
-              height: 30,
-              decoration: BoxDecoration(
-                color: Colors.pinkAccent,
-                borderRadius: BorderRadius.circular(30),
-              ),
-              child: Center(
-                child: Icon(
-                  Icons.location_on,
-                  color: Colors.white,
-                  size: 32,
-                ),
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 70, // default 56
+        leadingWidth: 70,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 10, top: 5, right: 2, bottom: 5,),
+          child: Container(
+            width: 45,
+            height: 45,
+            decoration: BoxDecoration(
+              color: Colors.pinkAccent,
+              shape: BoxShape.circle,
+            ),
+            child: Center(
+              child: Icon(
+                Icons.location_on,
+                color: Colors.white,
+                size: 32,
               ),
             ),
           ),
+        ),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+                "Delivery Address",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+                color: Colors.black54.withOpacity(0.6),
+              ),
+            ),Text(
+                "92, High Street, London",
+              style: TextStyle(
+                fontSize: 19,
+                fontWeight: FontWeight.w800
+              ),
+            ),
+          ],
         ),
       ),
     );
